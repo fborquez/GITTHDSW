61cfde69b3eab697bf720c0ae5c7ac2b95865ee8
fborquez <fborquez@CL-NBDLI7-44.tecnofarma.cl>
2020-05-26 20:01:48 -0400

    Primer Commit

---
+++ FJSF/src/cl/usm/PBean.java
@@ -0,0 +1,20 @@
+package cl.usm;
+
+public class PBean {
+	
+	private String saludo;
+	
+	public PBean(){
+		saludo="Hola!";
+	}
+	
+
+	public String getSaludo() {
+		return saludo;
+	}
+
+	public void setSaludo(String saludo) {
+		this.saludo = saludo;
+	}
+
+}
